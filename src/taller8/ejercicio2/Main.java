6622dedfcdd241308b3d23bc74b91ffa3a50ae5c
Gabriela Porras <gporrasb@unicartagena.edu.co>
2026-05-26 16:02:10 -0500

    feat(taller8-ejercicio2): implementacion de herencia con persona y estudiante

---
+++ src/taller8/ejercicio2/Main.java
@@ -0,0 +1,11 @@
+package taller8.ejercicio2;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Estudiante unEstudiante = new Estudiante("Ana Lopez", 20, "2026-ENG-001");
+
+        System.out.println("--- Detalles del Estudiante ---");
+        unEstudiante.mostrarDetalles();
+    }
+}
